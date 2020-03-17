61b7fc0486b39402e7c1dbbcf4f95bd8b08c5c2b
HarlandLohora <harland@lohora.com>
2020-03-16 21:56:46 -0300

    Form Validator complete

--- FormValidator/script.js
+++ FormValidator/script.js
@@ -18,25 +18,49 @@ function showSuccess(input){
 
 function isValidEmail(email){
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if(re.test(String(email.value.trim()).toLowerCase())){
+    showSuccess( email )
+  }else{
+    showError( email, 'Email is not valid' )
+  }
 }
 
 function checkRequired(inputs){
   inputs.forEach( input => {
-    console.log(input);
     if( input.value.trim() === '' ){
-      showError(input, getFieldName( input ))
+      showError(input, getFieldName( input, 'is required.' ))
     }else{
       showSuccess(input)
     }
   })
 }
 
-function getFieldName( input ){
-  return `${input.id.charAt(0).toUpperCase()}${ input.id.slice(1) } is required`
+function getFieldName( input, msg ){
+  return `${input.id.charAt(0).toUpperCase()}${ input.id.slice(1) } ${ msg }`
 }
+
+function checkLength( input, min, max ){
+  if( input.value.length < min ){
+    showError( input, getFieldName( input,`At least ${min} characters.` ) )
+  }else if( input.value.length > max ){
+    showError( input, getFieldName( input,`Max ${max} characters.` ) )
+  }else{
+    showSuccess(input)
+  }
+}
+
+function checkPasswordsMatch( password,password2 ){
+  if( password.value !== password2.value ){
+    showError( password2, 'Passwords do not match.' )
+  }
+}
+
 
 form.addEventListener('submit', e => {
   e.preventDefault();
   checkRequired([username,email,password,password2])
+  checkLength( username, 3, 6 )
+  checkLength( password, 4, 25 )
+  isValidEmail( email )
+  checkPasswordsMatch( password,password2 )
 })
